7372742b9051603f820e4277cdddcdf5e418a67a
Tiago Azevedo <tiagogadenz@icloud.com>
2024-10-01 14:04:38 -0300

    learning about server and client components and creating my very first record

---
+++ snippets/src/app/snippets/new/page.tsx
@@ -0,0 +1,44 @@
+import { db } from "@/db";
+import  { redirect } from "next/navigation" 
+
+export default function SnippetCreatePage() {
+  async function createSnippet(formData: FormData) {
+    // This need to be a server action
+    "use server";
+    
+    // check the users input
+    const title = formData.get("title") as string;
+    const code = formData.get("code") as string;
+
+    // creater a new record in the database
+    const snippet = await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+
+    console.log("Snippet created", snippet);
+    // redirect the user to the homepage
+    redirect("/");
+  }
+
+  return (
+    <form action={createSnippet}>
+      <h3 className="font-bold m-3">Create a Snippet</h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="title">Title</label>
+          <input type="text" name="title" className="border-rounded p-2 w-full" id="title" />
+        </div>
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="code">Code</label>
+          <textarea name="code" className="border-rounded p-2 w-full" id="code" />
+        </div>
+        <button type="submit" className="rounded p-2 bg-blue-200 font-semibold">
+          Create
+        </button>
+      </div>
+    </form>
+  );
+}
